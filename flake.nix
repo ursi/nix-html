4ab495905d5aad0f7f72894130806a4dae9f1f88
Mason Mackaman <masondeanm@aol.com>
2022-02-10 16:22:44 -0500

    Add deadnix for linting

--- flake.nix
+++ flake.nix
@@ -1,1 +1,21 @@
-{ outputs = { ... }: { __functor = _: { pkgs }: import ./. pkgs; }; }
+{ inputs =
+    { deadnix.url = "github:astro/deadnix";
+      make-shell.url = "github:ursi/nix-make-shell/1";
+      nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+      utils.url = "github:ursi/flake-utils/8";
+    };
+
+  outputs = { nixpkgs, utils, ... }@inputs:
+    { __functor = _: { pkgs }: import ./. pkgs; }
+    // (utils.apply-systems { inherit inputs; }
+          ({ deadnix, make-shell, ...}:
+             { devShell =
+                 make-shell
+                   { packages = [ deadnix ];
+                     aliases.lint = ''find -name "*.nix" | xargs deadnix'';
+                   };
+
+             }
+          )
+       );
+}
