ba958dc6d2efeea6a52b8da0f7c2bb150ffec6f4
Mason Mackaman <masondeanm@aol.com>
2022-05-02 16:28:21 -0400

    Change make-builder to make-site
    This also changes it to a derivation instead of a function that takes a
    path and builds things there. I have decided to do this for 2 reasons:
    1. I makes naming things easier
    2. It feels more like what people would expect to use, probably
       including myself as well

--- default.nix
+++ default.nix
@@ -16,14 +16,14 @@ pkgs:
 
     map = map: b: b // { ${map-attribute} = map; };
 
-    make-builder =
+    make-site =
       { args =
           [ { name = "dir";
               description = "The directory that contains the source files";
             }
 
             { name = "spec";
-              description = "An attrset whose keys denote targets and whose values are attrsets themselves, whose keys denote file extensions that will be compiled to the target extension. The values of these inner attrsets are the transformers that describe how to compile the source file into the target files.";
+              description = "An attrset whose keys denote target file extensions and whose values are attrsets themselves, whose keys denote file extensions that will be compiled to the target extension. The values of these inner attrsets are functions that describe how to compile the source files into the target files.";
             }
           ];
 
@@ -31,71 +31,72 @@ pkgs:
           ''
           Example usage:
           ```
-          let
-            build-site =
-              make-builder
-                ./website
-                (basic { args = { inherit social templates; }; })
-          in
-          pkgs.runCommand "website" {} (build-site "$out")
+          make-site
+            ./website
+            { html =
+                basic { args = { inherit social templates; }; }
+                // { md = markdown-plugin; }
+            }
           ```
           '';
 
-        returns = "A function that takes in a string path, and outputs the Bash commands to build the website at that path";
+        returns = "A derivation for the website";
 
         __functor = _: dir: spec:
-          foldl'
-            (acc: a:
-               let
-                 target = a.name;
-                 builders = a.value;
-                 change-ext = files.change-extension target;
+          p.runCommand "site-from-${toString dir}" {}
+            (l.concatStringsSep "\n"
+               (builtins.map
+                  ({ target, builders }:
+                     let
+                       change-ext = files.change-extension target;
 
-                 paths =
-                   filter
-                     (path:
-                        builders?${files.extension path}
-                        || files.extension path == target
-                     )
-                     (files.recursive-list dir);
+                       paths =
+                         filter
+                           (path:
+                              builders?${files.extension path}
+                              || files.extension path == target
+                           )
+                           (files.recursive-list dir);
 
 
-                 build-file = path:
-                   let
-                     make-path = path':
-                       builtins.path
-                         { name = l.strings.sanitizeDerivationName (baseNameOf path');
-                           path = path';
-                         };
-                   in
-                   if files.extension path == target then
-                     p.runCommand (change-ext path) {} "ln -s ${make-path (dir + path)} $out"
-                   else
-                     builders.${files.extension path}
-                       { system = dir + path;
-                         site = path;
-                       };
+                       build-file = path:
+                         let
+                           make-path = path':
+                             builtins.path
+                               { name =
+                                   l.strings.sanitizeDerivationName (baseNameOf path');
 
-                 map' = builders.${map-attribute} or l.id;
-               in
-               target-dir:
-                 acc target-dir
-                 + l.concatMapStringsSep "\n"
-                     (path:
-                        let
-                          build-path = change-ext path;
-                          dir = target-dir + l.escapeShellArg (dirOf build-path);
-                          file = target-dir + l.escapeShellArg build-path;
-                        in
-                        ''
-                        mkdir -p ${dir}
-                        ln -s ${map' (build-file path)} ${file}
-                        ''
-                     )
-                     paths
-            )
-            (l.const "")
-            (l.mapAttrsToList l.nameValuePair spec);
+                                 path = path';
+                               };
+                         in
+                         if files.extension path == target then
+                           p.runCommand (change-ext path) {}
+                             "ln -s ${make-path (dir + path)} $out"
+                         else
+                           builders.${files.extension path}
+                             { system = dir + path;
+                               site = path;
+                             };
+
+                       map' = builders.${map-attribute} or l.id;
+                     in
+                     l.concatMapStringsSep "\n"
+                       (path:
+                          let
+                            build-path = change-ext path;
+                            dir = "$out" + l.escapeShellArg (dirOf build-path);
+                            file = "$out" + l.escapeShellArg build-path;
+                          in
+                          ''
+                          mkdir -p ${dir}
+                          ln -s ${map' (build-file path)} ${file}
+                          ''
+                       )
+                       paths
+                  )
+                  (l.mapAttrsToList (target: builders: { inherit target builders; }) spec)
+               )
+            );
       };
 
     reflect = args:
