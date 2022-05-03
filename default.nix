8a5a0192d92c2a310cc0d2fa781754c42c4be61a
Mason Mackaman <masondeanm@aol.com>
2022-05-02 23:51:50 -0400

    Refactor map

--- default.nix
+++ default.nix
@@ -3,7 +3,6 @@ pkgs:
   let
     l = p.lib; p = pkgs;
     files = import ./files.nix l;
-    map-attribute = "__nix-html-map";
   in
   rec
   { basic = args: reflect (l.const args);
@@ -43,7 +42,7 @@ pkgs:
 
         returns = "A attrset of builders that is the same as `builders`, but with their outputs run through `map`.";
 
-        __functor = _: map: b: b // { ${map-attribute} = map; };
+        __functor = _: map: l.mapAttrs (_: v: paths: map (v paths));
       };
 
     make-site =
@@ -107,8 +106,6 @@ pkgs:
                              { system = dir + path;
                                site = path;
                              };
-
-                       map' = builders.${map-attribute} or l.id;
                      in
                      l.concatMapStringsSep "\n"
                        (path:
@@ -119,7 +116,7 @@ pkgs:
                           in
                           ''
                           mkdir -p ${dir}
-                          ln -s ${map' (build-file path)} ${file}
+                          ln -s ${build-file path} ${file}
                           ''
                        )
                        paths
